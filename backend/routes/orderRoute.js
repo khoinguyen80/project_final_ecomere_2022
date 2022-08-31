a9c02e736656b4ad098cbc3ca878eeb3fe3b56d1
unknown <duongtrankhoinguyen1207@gmail.com>
2022-08-31 19:48:37 +0700

    feat: add api  update and delete order

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,6 +3,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const router = express.Router();
 
@@ -15,5 +18,14 @@ router
   .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
