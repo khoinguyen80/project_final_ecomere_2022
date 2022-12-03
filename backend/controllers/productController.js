741f8e00971379b2b5d6308c5afcbaf8e416d5af
unknown <duongtrankhoinguyen1207@gmail.com>
2022-12-03 14:03:53 +0700

    update prodcut controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -214,12 +214,21 @@ const ProductController = {
     );
 
     let avg = 0;
+
     reviews.forEach((rev) => {
       avg += rev.rating;
     });
 
-    const ratings = avg / reviews.length;
+    let ratings = 0;
+
+    if (reviews.length === 0) {
+      ratings = 0;
+    } else {
+      ratings = avg / reviews.length;
+    }
+
     const numOfReviews = reviews.length;
+
     await Product.findByIdAndUpdate(
       req.query.productId,
       {
